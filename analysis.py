2e9690e79c0d6137d353e1aaa81fd04b86dbf593
Badger-Tree <colleenhaukaas@gmail.com>
2026-04-01 15:53:02 -0700

    started work in pandas

--- analysis.py
+++ analysis.py
@@ -1,16 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from db import get_crashes, get_populations, get_crashes_per_municipality,get_crashes_per_100k
 
 
 raw_crashes = get_crashes()
-populations = get_populations()
-crashes_per_municipality = get_crashes_per_municipality()
-crashes_per_100k = get_crashes_per_100k()
-# head methods to see if they work, not important
-# print(raw_crashes.head())
-# print(populations.head())
-# print(crashes_per_municipality.head())
-# print(crashes_per_100k.head())
+
+print(raw_crashes.describe(include='all'))
+# print(raw_crashes.head(5))
+
+# print(f"shape: {raw_crashes.shape}")
+# aggregated = raw_crashes.groupby(by="municipality_name")
+# print(aggregated.head(10))
 
 
+# crashes_per_municipality.sort_values("total_crashes", ascending=False).head(10).plot.bar( x="municipality_name", y = "total_crashes")
+# plt.xlabel("Municipality")
+# plt.ylabel("Number of Crashes")
+# plt.title("Crashes per Municipality")
+# plt.show()
 
--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import db
 
 def main():
-    #create the population and crash tables
-    # db.create_crash_table()
-    # db.import_crash_csv("Southern Interior_Full Data_data.csv")
-    # db.import_population_csv("Population_Projections.csv")
+    def setup():
+        #create the population and crash tables
+        db.create_crash_table()
+        db.import_crash_csv("Southern Interior_Full Data_data.csv")
+        db.import_population_csv("Population_Projections.csv")
 
-    
-    # calculate crashes per 100k in each munipcipality
-    db.create_regional_crash_summaries()
-    db.create_crashes_per_100k()
+    def create_aggregations():
+        # calculate crashes per 100k in each munipcipality
+        db.create_regional_crash_summaries()
+        db.create_crashes_per_100k()
     
 if __name__ == "__main__":
     main()
